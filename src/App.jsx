ffe785c8880c98dcd23e6f3cb9c05471b8574016
Godden13 <chrissgodden@gmail.com>
2023-06-24 17:04:15 +0100

    update drink api

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import DrinkContext from './drinkContext';
+import Drinks from './drinks/Drinks';
 
 function App() {
+  const [drinks, setDrinks] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch('http://localhost:3000/drinks?apiKey=1')
+        .then((res) => res.json())
+        .then((res) => {
+          setDrinks([...res]);
+        });
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DrinkContext.Provider value={drinks}>
+        <Drinks />
+      </DrinkContext.Provider>
     </div>
   );
 }
